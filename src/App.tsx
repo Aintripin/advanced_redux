a490099048541e596677fb35015b32fbcc589f2d
AintripinKWX <yelisey_toyota@mail.ru>
2024-11-25 14:41:12 +0300

    typed, tripped and dipped

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {useAppDispatch, useAppSelector} from "./hooks/redux";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const {count} = useAppSelector(state => state.userReducer);
+
+    const {} = useAppSelector(state => state.userReducer.users);
+// const {} = useSelector(state => state.);
+//     const {increment} = userSlice.actions;
+    const dispatch = useAppDispatch();
+
+    // console.log(increment(5));
+
+    return (
+        <div className="App">
+            {/*<h1>{count}</h1>*/}
+
+            {/*<button onClick={() => dispatch(increment(10))}>Increment</button>*/}
+        </div>
+    );
 }
 
 export default App;
